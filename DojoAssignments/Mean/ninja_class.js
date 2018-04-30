7eb113dcbcb9a55df1fe1b122dc9bb77643e11d6
Xiaojing Liu <iceliuxj@gmail.com>
2018-04-29 19:28:12 -0700

    js ninja assignment

--- DojoAssignments/Mean/ninja_class.js
+++ DojoAssignments/Mean/ninja_class.js
@@ -5,16 +5,26 @@ function Ninja(name){
     this.health = 100;
 
     this.sayName = function(name){
-        console.log("My ninja name is" + this.name + "!")
+        console.log("My ninja name is" + this.name + "!");
     }
 
     this.showStats = function(name){
-        console.log("Name: " + this.name + ", Health: " + this.health + ", Speed:" + this.speed + ", Strength:" + this.strength)
+        console.log("Name: " + this.name + ", Health: " + this.health + ", Speed:" + this.speed + ", Strength:" + this.strength);
     }
 
     this.drinkSake = function(){
         this.health += 10;
-        console.log (this.health)
+        console.log (this.health);
+    }
+
+    this.punch = function(ninja){
+        ninja.health -= 5;
+        console.log(ninja.name + " was punched by " + this.name + "and lost 5 Health!");
+    }
+
+    this.kick = function(ninja){
+        ninja.lostHealth = 15 * this.strength;
+        console.log(ninja.name + " was kicked by " + this.name + " and lost " + ninja.lostHealth + "!");
     }
 }
 
@@ -23,3 +33,8 @@ var ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
+
+var blueNinja = new Ninja("Goemon");
+var redNinja = new Ninja("Bill Gates");
+redNinja.punch(blueNinja);
+blueNinja.kick(redNinja);
