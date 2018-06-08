aa0fc639491515a0073c9094283a56c1016a902e
Xiaojing Liu <iceliuxj@gmail.com>
2018-06-08 11:51:02 -0700

    first commit

---
+++ DojoAssignments/c#/viewmodel/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using viewmodel.Models;
+
+namespace viewmodel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Numbers()
+        {
+            ViewData["Message"] = "Here are some numbers!";
+
+            return View();
+        }
+
+        public IActionResult Users()
+        {
+            ViewData["Message"] = "Here are some Users!";
+            List<string> any = new List<string>();
+            any.Add("Sally");
+            any.Add("Billy");
+            any.Add("Jean");
+            any.Add("Shawn");
+            Users users = new Users(){
+                people = any
+            };
+            // users.people.Add("Sally");
+            // users.people.Add("Billy");
+            // users.people.Add("Jean");
+            // users.people.Add("Shawn");
+
+            return View(users);
+        }
+
+        public IActionResult user()
+        {
+            ViewData["Message"] = "Here is a User!";
+            User user = new User (){
+                Name = "shawn"
+            };
+
+            return View(user);
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DojoAssignments/c#/viewmodel/Models/User.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+namespace viewmodel.Models
+{
+    public class User
+    {
+        public string Name {get;set;}
+
+    }
+
+    public class Users
+    {
+        public List<string> people {get;set;}
+    }
+}
